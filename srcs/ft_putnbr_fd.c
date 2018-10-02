1a8bc219b6690ad32e70dc176cf7332a41d98ef5
thk2b <theokobb@gmail.com>
2018-10-01 19:45:41 -0700

    fix putnbr

--- srcs/ft_putnbr_fd.c
+++ srcs/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/16 21:25:42 by tkobb             #+#    #+#             */
-/*   Updated: 2018/09/16 21:48:42 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/10/01 19:40:58 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	ft_putnbr_fd(int n, int fd)
 	else
 		un = (unsigned long)n;
 	pow = 1;
-	while ((next_pow = 10 * pow) < un && next_pow)
+	while ((next_pow = 10 * pow) <= un && next_pow)
 		pow = next_pow;
 	while (pow)
 	{
