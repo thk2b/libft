1aa509919a98414c0b2da355cd5814845dfc850e
thk2b <theokobb@gmail.com>
2018-10-22 00:07:25 -0700

    test llist

--- tests/test_llist.c
+++ tests/test_llist.c
@@ -6,16 +6,90 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/21 23:02:37 by tkobb             #+#    #+#             */
-/*   Updated: 2018/10/21 23:25:19 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/10/22 00:06:55 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "linked_list.h"
 #include "libft.h"
 #include "tests.h"
+#include <assert.h>
 
-void test_llist(void)
+static int	cmp(void *ctx, void *data)
 {
-	
+	return (ft_strcmp(ctx, data) == 0);
 }
 
+int			g_delcount = 0;
+static void	del(void *data)
+{
+	(void)data;
+	g_delcount++;
+}
+
+static void	test0(void)
+{
+	t_llist			*head;
+	char			**strv;
+	t_llist_node	*found;
+
+	head = NULL;
+	strv = ft_strsplit("abc def ghi jkl mno", ' ');
+	llist_push(&head, strv[0]);
+	llist_push(&head, strv[1]);
+	llist_push(&head, strv[2]);
+	assert(head->len == 3);
+	assert(ft_strcmp(head->first->data, strv[0]) == 0);
+	assert(ft_strcmp(head->first->next->data, strv[1]) == 0);
+	assert(ft_strcmp(head->first->next->next->data, strv[2]) == 0);
+	found = llist_find(head, "ghi", cmp);
+	assert(ft_strcmp(found->data, "ghi") == 0);
+	free(strv);
+}
+
+#include <printf.h>
+static void	test1(void)
+{
+	t_llist	*head;
+	char	**strv;
+
+	head = NULL;
+	strv = ft_strsplit("abc def ghi jkl mno", ' ');
+	llist_shift(&head, strv[0]);
+	llist_shift(&head, strv[1]);
+	llist_shift(&head, strv[2]);
+	assert(head->len == 3);
+	llist_remove(head, "ghi", cmp, del);
+	assert(g_delcount == 1);
+	assert(ft_strcmp(head->first->data, strv[1]) == 0);
+	assert(ft_strcmp(head->first->next->data, strv[0]) == 0);
+	free(strv);
+}
+
+static void	test2(void)
+{
+	t_llist	*head;
+	char	**strv;
+
+	head = NULL;
+	strv = ft_strsplit("abc def ghi jkl mno", ' ');
+	llist_push(&head, strv[0]);
+	llist_push(&head, strv[1]);
+	llist_push(&head, strv[2]);
+	llist_shift(&head, strv[3]);
+	llist_shift(&head, strv[4]);
+	assert(head->len == 5);
+	assert(ft_strcmp((char*)head->first->data, strv[4]) == 0);
+	assert(ft_strcmp(head->first->next->next->data,
+		strv[0]) == 0);
+	assert(ft_strcmp(head->first->next->next->next->next->data,
+		strv[2]) == 0);
+	free(strv);
+}
+
+void		test_llist(void)
+{
+	test0();
+	test1();
+	test2();
+}
