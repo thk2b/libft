d805764ab2098034fc52ef67fae8705d4d54131e
thk2b <theokobb@gmail.com>
2018-10-24 09:38:40 -0700

    default del

--- srcs/priority_queue/pq_pop.c
+++ srcs/priority_queue/pq_pop.c
@@ -6,13 +6,17 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/22 23:34:02 by tkobb             #+#    #+#             */
-/*   Updated: 2018/10/22 23:59:51 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/10/24 09:37:57 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "priority_queue.h"
 
-void	*pq_pop(t_pq **pq, t_pq_del_fn del)
+static void	default_del(void *data)
+{
+	free(data);
+}
+void		*pq_pop(t_pq **pq, t_pq_del_fn del)
 {
 	t_pq	*ret;
 	void	*data;
@@ -20,6 +24,8 @@ void	*pq_pop(t_pq **pq, t_pq_del_fn del)
 	ret = *pq;
 	if (ret == NULL)
 		return (NULL);
+	if (del == NULL)
+		del = default_del;
 	*pq = ret->next;
 	data = ret->data;
 	del(ret);
