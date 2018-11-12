9fce785bcb1b12f962437696da8c84e2c6e99477
thk2b <theokobb@gmail.com>
2018-11-12 00:38:41 -0800

    fix push

--- srcs/clist/clist_push.c
+++ srcs/clist/clist_push.c
@@ -6,7 +6,7 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/11 22:59:16 by tkobb             #+#    #+#             */
-/*   Updated: 2018/11/11 23:14:25 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/11/12 00:35:12 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ int		clist_push(t_clist **cl, void *data)
 	}
 	else
 	{
-		n->prev = (*cl);
-		n->next = (*cl)->next;
-		(*cl)->next->prev = n;
-		(*cl)->next = n;
+		n->next = (*cl);
+		n->prev = (*cl)->prev;
+		(*cl)->prev->next = n;
+		(*cl)->prev = n;
 	}
 	*cl = n;
 	return (0);
