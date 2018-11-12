7ac3d9c39642cf72a6739aa3da2a62ffa45a09b1
thk2b <theokobb@gmail.com>
2018-11-11 22:16:41 -0800

    add clist

---
+++ includes/clist.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clist.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/11 22:08:37 by tkobb             #+#    #+#             */
+/*   Updated: 2018/11/11 22:08:37 by tkobb            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLIST_H
+# define CLIST_H
+
+typedef struct	s_clist
+{
+	struct s_clist	*prev;
+	struct s_clist	*next;
+	void			*data;
+}				t_clist;
+
+typedef void	(*t_clist_del_fn)(void *);
+typedef int		(*t_clist_cmp_fn)(void *, void *);
+
+t_clist			*clist_new(void *data);
+void			clist_free(t_clist *cl, t_clist_del_fn del);
+int				clist_push(t_clist **cl, void *data);
+void			*clist_pop(t_clist **cl);
+int				clist_rotate(t_clist **cl);
+int				clist_rrotate(t_clist **cl);
+int				clist_swap_nxt(t_clist **cl);
+int				clist_is_sorted(t_clist *clist, t_clist_cmp_fn cmp);
+
+#endif
